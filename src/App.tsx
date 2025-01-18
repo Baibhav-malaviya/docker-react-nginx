53e167b6529b98d48a0a8971dbe8bc8e5a898a70
Baibhav Kumar <baibhav.kr73@gmail.com>
2025-01-19 01:47:10 +0530

    testing for github action

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 const App: React.FC = () => {
-	return <div>Hello world v4</div>;
+	return (
+		<div className="min-h-screen w-full bg-yellow-50 items-center justify-center">
+			<h1 className="text-3xl text-violet-600">Hello world</h1>
+		</div>
+	);
 };
 
 export default App;
