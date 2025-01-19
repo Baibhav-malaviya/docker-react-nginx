f5fdb6ad85d57b4f31a0153767d73845fda7bf64
Baibhav Kumar <baibhav.kr73@gmail.com>
2025-01-19 19:45:25 +0530

    problem with aws-ecr push job

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 const App: React.FC = () => {
 	return (
-		<div className="items-center justify-center w-full min-h-screen bg-yellow-50">
+		<div className="items-center justify-center w-full min-h-screen gap-5 bg-yellow-50">
 			<h1 className="text-3xl text-violet-600">Hello world</h1>
-			<p className="p-10 m-5 text-xl font-semibold text-center text-rose-500">
+			<p className="p-10 m-5 text-xl font-semibold text-center text-rose-500 bg-rose-50">
 				WE are on <span className="text-lg italic">aws-ecr</span> and as-well-as{" "}
 				<span className="text-lg italic">dockerHub</span>
 			</p>
+			<p className="p-10 m-5 text-xl font-semibold text-center text-rose-500 bg-rose-50"></p>
 		</div>
 	);
 };
