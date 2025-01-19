dce60d664a3632736be92b8ec9d4b4f1fe23dc50
Baibhav Kumar <baibhav.kr73@gmail.com>
2025-01-19 19:36:14 +0530

    testing for aws-ecr

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 const App: React.FC = () => {
 	return (
-		<div className="min-h-screen w-full bg-yellow-50 items-center justify-center">
+		<div className="items-center justify-center w-full min-h-screen bg-yellow-50">
 			<h1 className="text-3xl text-violet-600">Hello world</h1>
+			<p className="p-10 m-5 text-xl font-semibold text-center text-rose-500">
+				WE are on <span className="text-lg italic">aws-ecr</span> and as-well-as{" "}
+				<span className="text-lg italic">dockerHub</span>
+			</p>
 		</div>
 	);
 };
